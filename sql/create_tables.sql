15bbc72d7f9c4818c4946d240c497ab6706c918e
Bhajan-singh-kaniyal <kaniyalrahul240901@gmail.com>
2026-03-25 21:16:05 +0530

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,13 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(50),
+    customer_unique_id VARCHAR(50),
+    customer_city VARCHAR(50),
+    customer_state VARCHAR(50)
+);
 
+CREATE TABLE orders (
+    order_id VARCHAR(50),
+    customer_id VARCHAR(50),
+    order_status VARCHAR(20),
+    order_purchase_timestamp TIMESTAMP
+);
